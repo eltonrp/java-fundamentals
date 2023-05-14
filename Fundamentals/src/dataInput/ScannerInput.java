71227231e1fc55183f55f601d55ce87f651d9074
eltonrp <eltonrpo@gmail.com>
2023-05-14 13:28:48 -0300

    first commit

--- Fundamentals/src/dataInput/ScannerInput.java
+++ Fundamentals/src/dataInput/ScannerInput.java
@@ -1,9 +1,37 @@
 package dataInput;
 
+import java.util.Locale; //Needed import for Locale module
+import java.util.Scanner; //module import for data input
+
 public class ScannerInput {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US); //US number default definition
+
+		Scanner sc = new Scanner(System.in); //instantiate object for data input
+		
+		System.out.print("What is your name: ");
+		
+		String name = sc.next(); //string data input method
+		
+		System.out.println("Your name is: " + name + "!");
+		
+		System.out.println("Give me a integer number: ");
+		
+		int numInt = sc.nextInt(); //integer number input method
+		
+		System.out.println("The int number is: " + numInt);
+		
+		System.out.println("Give me a double number: ");
+		
+		double numDouble = sc.nextDouble(); //double number input method, same about float
+		
+		System.out.printf("The double number is: %.2f%n", numDouble);
+		
+		sc.close();
+		
+		System.out.printf("%nThis is the end!!!");
 
 	}
 
